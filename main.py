6c97dcc11ab904df1aa7aa5376fae7063e5af995
sexy-code-maker <choidonghun0831@gmail.com>
2023-06-08 12:35:20 +0900

    wmsgpt_web

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template,abort,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    abort(503)
+    return render_template("index.html")
+
+@app.route('/github')
+def github():
+    return redirect('https://github.com/sexy-code-maker/WMSGPT')
+
+@app.route("/admin")
+def nicetry():
+    return "<h1>NICE TRY HAHA. 나약하구만!</h1>"
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0',port = 3000,debug = 1)
