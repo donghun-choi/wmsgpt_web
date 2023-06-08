fe6bb6666b53f5c7e56bf8a8e5e3e7717c56cde5
sexy-code-maker <choidonghun0831@gmail.com>
2023-06-09 02:36:52 +0900

    fixed bugs, phone pwa

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -28,39 +28,6 @@ $(function () {
   });
 });
 
-// modern Chrome requires { passive: false } when adding event
-var supportsPassive = false;
-try {
-  window.addEventListener(
-    "test",
-    null,
-    Object.defineProperty({}, "passive", {
-      get: function () {
-        supportsPassive = true;
-      },
-    })
-  );
-} catch (e) {}
-
-var wheelOpt = supportsPassive ? { passive: false } : false;
-var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
-
-// call this to Disable
-function disableScroll() {
-  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
-  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
-  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
-}
-
-// call this to Enable
-function enableScroll() {
-  window.removeEventListener("DOMMouseScroll", preventDefault, false);
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
-  window.removeEventListener("touchmove", preventDefault, wheelOpt);
-  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
-}
-
 var $html = $("html");
 var $sections = $(".content");
 var page = 0;
@@ -82,3 +49,15 @@ $(document).on("keydown", function (e) {
     scrollToPage(page - 1);
   }
 });
+
+var updateCurrentPage = function () {
+  var scrollTop = $html.scrollTop();
+  var pageIndex = Math.floor(scrollTop / $(window).height());
+
+  if (pageIndex !== page) {
+    page = pageIndex;
+    console.log("Current page:", page);
+  }
+};
+
+$(window).on("scroll", updateCurrentPage);
