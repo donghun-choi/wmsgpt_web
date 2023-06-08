b9abee37d5ad2b99029781b5ba59956fffd4f333
sexy-code-maker <choidonghun0831@gmail.com>
2023-06-08 22:41:35 +0900

    wip

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    abort(503)
+    # abort(503)
     return render_template("index.html")
 
 @app.route('/github')
@@ -14,5 +14,7 @@ def github():
 @app.route("/admin")
 def nicetry():
     return "<h1>NICE TRY HAHA. 나약하구만!</h1>"
+
 if __name__ == "__main__":
+    # app.run(debug = 1)
     app.run(host = '0.0.0.0',port = 3000,debug = 1)
--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,5 +1,5 @@
 const inner = document.querySelector(".inner");
-const section = document.querySelector("section");
+const section = document.querySelector("#sec-1");
 
 window.onscroll = function () {
   let value = window.pageYOffset / section.offsetTop + 1;
@@ -10,7 +10,7 @@ $(function () {
   var $window = $(window); //Window object
 
   var scrollTime = 1.2; //Scroll time
-  var scrollDistance = 170; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+  var scrollDistance = 1000; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
 
   $window.on("mousewheel DOMMouseScroll", function (event) {
     event.preventDefault();
@@ -26,4 +26,59 @@ $(function () {
       overwrite: 5,
     });
   });
+});
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      },
+    })
+  );
+} catch (e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+var $html = $("html");
+var $sections = $(".content");
+var page = 0;
+
+var scrollToPage = function (pageNum) {
+  if (pageNum < 0 || pageNum >= $sections.length) return;
+
+  page = pageNum;
+  var posTop = page * $(window).height();
+  $html.animate({ scrollTop: posTop });
+};
+
+$(document).on("keydown", function (e) {
+  if ($html.is(":animated")) return;
+
+  if (e.key === "ArrowDown") {
+    scrollToPage(page + 1);
+  } else if (e.key === "ArrowUp") {
+    scrollToPage(page - 1);
+  }
 });
